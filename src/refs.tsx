482472e68cc1ff205bc630254b928bcbbab7bf15
sny430 <shinya430.g@gmail.com>
2023-01-22 14:47:29 +0900

    :technologist: update url refs

--- src/refs.tsx
+++ src/refs.tsx
@@ -1,4 +1,3 @@
-// export const ClientURL = 'https://gray-ground-071c64d00.2.azurestaticapps.net'
-export const ClientURL = 'http://localhost:3000'
+export const ClientURL = 'https://gray-ground-071c64d00.2.azurestaticapps.net'
 export const ServerURL = 'https://nokogiri-backend.onrender.com'
 export const RecordId = '8f70d286-ecfe-4a10-84ba-037bee2b0af3'
