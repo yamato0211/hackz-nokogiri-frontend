1579f4bb3e52fc34db3000000e732abe3145944d
yamato0211 <kumayama0211@gmail.com>
2023-01-22 14:29:22 +0900

    :sparkles: add Member Page
    add Member Page

--- src/pages/activity/[aId].tsx
+++ src/pages/activity/[aId].tsx
@@ -9,12 +9,34 @@ interface UserData {
   lineID: string
 }
 
+interface Member {
+  lineId: string
+  activitieId: string
+}
+
+interface Record {
+  id: string
+  name: string
+  lineId: string
+}
+
+interface Activity {
+  id: string
+  name: string
+  dateTime: string
+  place: string
+  misc: string
+  createdAt: string
+  updatedAt: string
+  member: Member[]
+  Record: Record
+}
+
 const Activity = () => {
   const router = useRouter()
   const { aId } = router.query
-
   const [user, setUser] = useState<UserData>()
-  const [activity, setActivity] = useState()
+  const [activity, setActivity] = useState<Activity>()
 
   useEffect(() => {
     const getUserInfo = async () => {
@@ -27,20 +49,49 @@ const Activity = () => {
       params_second.append("id_token", id_token)
       params_second.append("client_id", process.env.NEXT_PUBLIC_CLIENT_ID as string)
       const userdata = await axios.post("https://api.line.me/oauth2/v2.1/verify", params_second);
-      console.log(userdata.data)
-      console.log("userpicture: " + userdata.data.picture)
       setUser({
         name: userdata.data.name,
         picture: userdata.data.picture,
         lineID: userdata.data.sub
       })
+      console.log(userdata.data)
 
-      const response = await axios.get(ServerURL + "/activitie/" + aId)
-      setActivity(response.data)
+      await axios.get(ServerURL + "/activitie/" + aId as string)
+      .then((res) => {
+        setActivity(res.data)
+      })
+
+      console.log(activity)
     }
-  })
+    if(router.isReady) {
+      getUserInfo()
+    }
+  },[router.isReady])
 
-  return <p>Activity: {aId}</p>
+  async function addMember() {
+    const response = await axios.get(ServerURL + "/activitie/" + `?activitieId=${aId}&lineId=${user?.lineID}`)
+    console.log(response.data)
+
+    if(response.status === 200){
+      alert('done!')
+      router.push('/')
+    }else{
+      alert('failed!')
+      router.push('/')
+    }
+  }
+
+  return (
+    <div>
+      <h1>{activity?.name}</h1>
+      <h1>{activity?.place}</h1>
+      <h1>{activity?.dateTime}</h1>
+      <h1>{activity?.misc}</h1>
+      <button onClick={addMember}>参加する</button>
+      <br />
+      <button onClick={() => {router.push('/')}}>参加しない</button>
+    </div>
+  )
 }
 
 export default Activity
