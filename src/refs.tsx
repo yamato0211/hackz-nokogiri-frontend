26ae260c08ac5f0c1550e98f47e1feae0281e261
sny430 <shinya430.g@gmail.com>
2023-01-22 10:49:36 +0900

    :bug: fix url refs

--- src/refs.tsx
+++ src/refs.tsx
@@ -1,3 +1,3 @@
-export const ClientURL = 'https://green-beach-0a9c51000.2.azurestaticapps.net/'
-export const ServerURL = 'https://nokogiri-backend.onrender.com/'
+export const ClientURL = 'https://green-beach-0a9c51000.2.azurestaticapps.net'
+export const ServerURL = 'https://nokogiri-backend.onrender.com'
 export const RecordId = 'e3539cb0-ad08-48f2-936a-45d4eb393e4b'
